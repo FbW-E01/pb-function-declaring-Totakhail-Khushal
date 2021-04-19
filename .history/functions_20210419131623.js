2b66e81c26c24530ce1177ef8ec5535e56a3fa2d
khushal <totakhailkhushal@gmail.com>
2021-04-19 14:14:58 +0200

    function declaration

---
+++ .history/functions_20210419131623.js
@@ -0,0 +1,19 @@
+// # Declaring Functions 
+
+// #### 1. Multiply - Function Declaration
+//     * Create a function that multiples a number by another number.
+function multiply(number1, number2) {
+    console.log(number1 * number2);
+
+}
+multiply(3, 5);
+
+// #### 2. Multiply - Function Declarations as Values
+//     * Rework the syntax of the above function so that the function declaration is stored as a value.
+
+// #### 3. Multiply - Arrow Function
+//     * Rework the syntax of the function declaration so that is uses the arrow function shorthand.
+
+// #### 4. Declarations
+//     * Create functions(using all three declarations) to check the remainder of division given two numbers.\
+
